228b5770fafc5ef63ff56c286cd201c68eff4216
mohd danish <mddanishyusuf@gmail.com>
2020-10-30 10:47:31 +0530

    banner replaced

--- src/components/layout.js
+++ src/components/layout.js
@@ -21,7 +21,7 @@ const Layout = ({ children }) => {
 
     return (
         <>
-        <div className="banner" style={{backgroundColor: '#ffcd57', textAlign: 'center', lineHeight: '16px !important'}}><p style={{marginBottom: 0}}><span style={{backgroundColor: "#000", padding: "2px 5px", color: '#fff', fontSize: 11, marginRight: 10, borderRadius: 4}}>new</span><a  style={{color: '#000'}} href="https://public-apis.io/learn" target="_blank">API courses for begineers</a></p></div>
+        <div className="banner" style={{backgroundColor: '#ffcd57', textAlign: 'center', lineHeight: '16px !important'}}><p style={{marginBottom: 0}}><span style={{backgroundColor: "#000", padding: "2px 5px", color: '#fff', fontSize: 11, marginRight: 10, borderRadius: 4}}>NoCodeAPI</span><a  style={{color: '#000'}} href="https://nocodeapi.com" target="_blank">Connect with third-party APIs</a></p></div>
         <div className="main-conatiner">
             <Header siteTitle={data.site.siteMetadata.title} />
             <div>
