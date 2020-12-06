a6b019093e0ea8748a2a75587feb49e37a2f984e
mohd danish <mddanishyusuf@gmail.com>
2020-12-06 15:14:09 +0530

    fixed

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
     },
     plugins: [
         `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-netlify`,
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
         {
